73fa7b7dac0bfa2e8e288c06fa503295314e6890
camilntl <91890013+camilntl@users.noreply.github.com>
2023-11-14 09:14:31 +0100

    camil

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -9,38 +9,90 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Message;
+use App\Entity\User;  
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+
 
 class ChatController extends AbstractController
 {
-    /**
+      /**
      * @Route("/chat", name="chat")
      */
-    public function chat()
-    {
-        return $this->render('chat/chat.html.twig');
+
+     public function chat(Request $request, EntityManagerInterface $entityManager, Security $security)
+{
+    $message = new Message();
+    $form = $this->createFormBuilder($message)
+        ->add('Content', TextType::class)
+        ->add('send', SubmitType::class, ['label' => 'Envoyer'])
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        
+            $data = json_decode($request->getContent(), true);
+            $user = $security->getUser();
+    
+            if (!$user) {
+                return new JsonResponse(['status' => 'error', 'message' => 'Utilisateur non authentifié'], 403);
+            }
+    
+            $message = new Message();
+            $message->setContent($data['message']);
+            $message->setCreatedAt(new \DateTimeImmutable());
+            $message->setUserId($user->getId());
+            $message->setConversationId($data['conversationId']);
+    
+            $entityManager->persist($message);
+            $entityManager->flush();
+    
+            return new JsonResponse(['status' => 'success']);
+        
     }
 
+    return $this->render('chat/chat.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
+
+
     /**
-     * @Route("/get-messages", name="get_messages")
+     * @Route("/get-messages/{conversationId}", name="get_messages")
      */
-    public function getMessages(SessionInterface $session)
+    public function getMessages($conversationId, EntityManagerInterface $entityManager, Security $security)
     {
-        $messages = $session->get('chat_messages', []);
+        $user = $security->getUser();
+        if (!$user) {
+            return new JsonResponse(['status' => 'error', 'message' => 'Utilisateur non authentifié'], 403);
+        }
 
-        return new JsonResponse($messages);
+        $messageRepository = $entityManager->getRepository(Message::class);
+        $messages = $messageRepository->findBy(['conversation' => $conversationId]);
+
+        $messagesArray = [];
+        foreach ($messages as $message) {
+            $messagesArray[] = [
+                'id' => $message->getId(),
+                'content' => $message->getContent(),
+                // Ajoutez d'autres propriétés nécessaires
+            ];
+        }
+
+        return new JsonResponse($messagesArray);
     }
 
-    /**
+
+
+    
+    /*/**
      * @Route("/send-message", name="send_message", methods={"POST"})
      */
-    public function sendMessage(Request $request, SessionInterface $session)
-    {
-        $data = json_decode($request->getContent(), true);
-
-        $messages = $session->get('chat_messages', []);
-        $messages[] = $data['message'];
-        $session->set('chat_messages', $messages);
-
-        return new JsonResponse(['status' => 'success']);
-    }
+   
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -43,6 +43,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this->email;
     }
 
+    
     /**
      * A visual identifier that represents this user.
      *
@@ -83,6 +84,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    
 
     public function getFirstName(): ?string
     {
